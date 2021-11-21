306d3a68203e7fc6400380ab3ab66786a40a426e
NaN <47554753+thrfur@users.noreply.github.com>
2021-11-21 23:18:17 +0800

    ADD test package WITH ADD Test1

---
+++ src/com/javaCase/test/Test1.java
@@ -0,0 +1,42 @@
+package com.javaCase.test;
+
+import java.util.*;
+
+/**
+ * @author THRFUR
+ * 本地测试，与项目无关
+ */
+
+public class Test1 {
+    public static void main(String[] args) {
+        Person p1 = new Person("THRFUR", 2);
+        Map<Person, Integer> map = new HashMap<>();
+        map.put(p1,123);
+        Person p2 = new Person("THRFUR", 2);
+        System.out.println(p1 == p2);  //false
+        System.out.println(p1.equals(p2));  //true
+        System.out.println(map.get(p1));  //123
+        System.out.println(map.get(p2));  //123
+
+    }
+}
+
+class Person {
+    private String name;
+    private int age;
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public boolean equals(Object obj) {
+        if (obj instanceof Person) {
+            Person p = (Person) obj;
+            return Objects.equals(this.name, p.name) && this.age == p.age;
+        } else return false;
+    }
+
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+}
