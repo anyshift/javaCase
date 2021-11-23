2bdc5653a921dba25fa7a1393bf64474bdf74c4e
NaN <47554753+thrfur@users.noreply.github.com>
2021-11-23 23:39:42 +0800

    UPDATE EnumMapTest_1

--- src/com/javaCase/TEST/TEST_1/EnumMapTest_1.java
+++ src/com/javaCase/TEST/TEST_1/EnumMapTest_1.java
@@ -8,14 +8,14 @@ import java.util.Map;
  * 本地测试，与项目无关：EnumMap的使用
  */
 
-public class EnumTest_1 {
+public class EnumMapTest_1 {
     public static void main(String[] args) {
        Map<Person, String> map = new EnumMap<>(Person.class);
        map.put(Person.P1, "第一个人");
        map.put(Person.P2, "第二个人");
        map.put(Person.P3, "第三个人");
-        System.out.println(map);
-        System.out.println(map.get(Person.P1));
+       System.out.println(map);
+       System.out.println(map.get(Person.P1));
     }
 }
 
