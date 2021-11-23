ca767e55e06fb28b1ba1032cae816d50e78ee7c8
NaN <47554753+thrfur@users.noreply.github.com>
2021-11-23 23:01:27 +0800

    ADD EnumTest_1

---
+++ src/com/javaCase/TEST/TEST_1/EnumTest_1.java
@@ -0,0 +1,24 @@
+package com.javaCase.TEST.TEST_1;
+
+import java.util.EnumMap;
+import java.util.Map;
+
+/**
+ * @author THRFUR
+ * 本地测试，与项目无关：EnumMap的使用
+ */
+
+public class EnumTest_1 {
+    public static void main(String[] args) {
+       Map<Person, String> map = new EnumMap<>(Person.class);
+       map.put(Person.P1, "第一个人");
+       map.put(Person.P2, "第二个人");
+       map.put(Person.P3, "第三个人");
+        System.out.println(map);
+        System.out.println(map.get(Person.P1));
+    }
+}
+
+enum Person {
+    P1, P2, P3;
+}
