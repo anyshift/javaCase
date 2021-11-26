8dbbf432896302533c90137ce97da3f7cb509382
NaN <47554753+thrfur@users.noreply.github.com>
2021-11-27 00:07:06 +0800

    UPDATE TEST

--- src/com/javaCase/TEST/TEST_4/TEST.java
+++ src/com/javaCase/TEST/TEST_4/TEST.java
@@ -1,5 +1,6 @@
 package com.javaCase.TEST.TEST_4;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
@@ -21,8 +22,15 @@ public class TEST {
     }
     static List<Message> process(List<Message> received) {
         // TODO: 按sequence去除重复消息
-
-        return received;
+        List<Message> receiveds = new ArrayList<Message>(); //空Message类型的ArrayList集合
+        Set<Integer> set = new TreeSet<Integer>(); //空Integer类型的TreeSet集合
+        for (Message message: received) {  //把形参传来的received遍历给同样是Message类型的message
+            if (!set.contains(message.sequence)) {  //如果set集合里面没有相同的sequence，则执行
+                set.add(message.sequence); //将不重复的sequence(Key值)添加进set中
+                receiveds.add(message); //将不重复的received添加到receiveds中
+            }//如果set集合里存在相同的sequence，则不执行
+        }
+        return receiveds; //返回ArrayList集合
     }
 }
 
