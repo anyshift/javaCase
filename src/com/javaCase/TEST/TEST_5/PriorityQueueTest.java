c9ccd75760c4abca9a0183ca7f2f671f130f4218
NaN <47554753+thrfur@users.noreply.github.com>
2021-11-30 17:21:58 +0800

    ADD PriorityQueueTest

---
+++ src/com/javaCase/TEST/TEST_5/PriorityQueueTest.java
@@ -0,0 +1,57 @@
+package com.javaCase.TEST.TEST_5;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+/**
+ * @author THRFUR
+ * 本地测试，与项目无关：优先级队列PriorityQueue的使用
+ */
+
+public class PriorityQueueTest {
+    public static void main(String[] args) {
+        Queue<User> q = new PriorityQueue<>(new UserComparator());
+        // 添加3个元素到队列:
+        q.offer(new User("Bob", "A10"));
+        q.offer(new User("Alice", "A2"));
+        q.offer(new User("Boss", "V1"));
+        System.out.println(q.poll()); // Boss/V1
+        System.out.println(q.poll()); // Bob/A1
+        System.out.println(q.poll()); // Alice/A2
+        System.out.println(q.poll()); // null,因为队列为空
+    }
+}
+
+class UserComparator implements Comparator<User> {
+    public int compare(User u1, User u2) {
+        if (u1.number.charAt(0) == u2.number.charAt(0)) {
+            // 如果两人的号都是A开头或者都是V开头,比较号的大小:
+            if (u1.number.length() == u2.number.length()) {
+                return u1.number.compareTo(u2.number);
+            } else return u1.number.length() - u2.number.length();
+        }
+
+        if (u1.number.charAt(0) == 'V') {
+            // u1的号码是V开头,优先级高:
+            return -1;
+        } else {
+            return 1;
+        }
+
+    }
+}
+
+class User {
+    public final String name;
+    public final String number;
+
+    public User(String name, String number) {
+        this.name = name;
+        this.number = number;
+    }
+
+    public String toString() {
+        return name + "/" + number;
+    }
+}
