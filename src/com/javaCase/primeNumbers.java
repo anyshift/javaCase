e48610ac29bff3bfa455cb0538c1ee621c3245f5
THRFUR <thrfurs@gmail.com>
2022-01-17 17:08:58 +0800

    ADD primeNumbers

---
+++ src/com/javaCase/primeNumbers.java
@@ -0,0 +1,26 @@
+package com.javaCase;
+
+/**
+ * @author THRFUR
+ * 求质数
+ */
+
+public class primeNumbers {
+    public static void main(String[] args) {
+        getPrimeNumbers(1000);
+    }
+    public static void getPrimeNumbers(int num){
+        for (int i = 2; i < num; i++){
+            boolean flag = true;
+            for (int j = 2; j <= Math.sqrt(i); j++){
+                if (i % j == 0) { //如果可以被j整除
+                    flag = false;  //则i不是质数
+                    break; //终止此次循环
+                }
+            }
+            if (flag){
+                System.out.println(i);
+            }
+        }
+    }
+}
